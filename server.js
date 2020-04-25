98294614b21beb061fe0cc4af3dfeb38dede24a2
Bim <minhle@Bims-MacBook-Pro.local>
2020-04-25 15:51:35 +0700

    fixed database and register signin endpoints

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = knex({
     host:'127.0.0.1',
     user: 'minhle',
     password:'',
-    database:'testflow'
+    database:'flowtest'
   }
 });
 
@@ -21,42 +21,47 @@ app.use(bodyParser.json());
 
 //registering 
 app.post('/register', (req, res) => {
-  const {email, password} = req.body;
+  const {username, email, password} = req.body;
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
     db.transaction(trx => {
       trx.insert({
+        username: username,
         hash: hash,
         email: email
       })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
+      .into('account')
+      .returning('*')
+      .then(user => {
+        return trx('member')
+        .returning('*')
+        .insert({
+          memberid: user[0].id,
+          name: username,
+          tripid: null
         })
+        .then(user => {
+          res.json(user[0]);
+          console.log(user[0]);
+        })
+        .catch(err => res.status(400).json('Username/ Email already taken'))
       })
       .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register')) 
+    .catch(err => res.status(400).json(err)) 
 })
 
 //sign in
-/*app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
+app.post('/signin', (req, res) => {
+  db.select('email', 'hash', 'id').from('account')
     .where('email', '=', req.body.email)
     .then(data => {
       const valid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const id = data[0].id;
       if (valid) {
-        return db.select('*').from('users')
-        .where('email', '=', req.body.email)
+        return db.select('*').from('member')
+        .where('memberid', '=', id)
         .then(user => {
           res.json(user[0])
         })
@@ -67,30 +72,5 @@ app.post('/register', (req, res) => {
     })
     .catch(err => res.status(400).json('wrong email/password'))
 })
-*/
-
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-  .where('email', '=', req.body.email)
-  .then(data => {
-    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-    if (isValid) {
-      return db.select('*').from('users')
-      .where('email', '=', req.body.email)
-      .then(user => {
-        res.json(user[0])
-      })
-      .catch(err => res.status(400).json('unable to get user'))
-    }
-    else 
-      res.status(400).json('wrong credentials')
-  })
-  .catch(err=> res.status(400).json('wrong credentials'))
-})
 
 app.listen(3000, () => console.log(' app running on port 3000!'))
-
-
-
-// login: username, email, hash
-// users: username, email, joined
